9302824c66bc3de389425bd20f84c88b0d4d2012
Benoit Larroque <zeta.ben@gmail.com>
2012-08-08 14:07:56 +0200

    Update new parser to the same feature set

--- lib/yard-rspec/handler.rb
+++ lib/yard-rspec/handler.rb
@@ -8,7 +8,7 @@ class RSpecDescribeHandler < YARD::Handlers::Ruby::Base
       objname += (src[0] == "#" ? "" : "::") + src
     end
     obj = {:spec => owner ? (owner[:spec] || "") : ""}
-    obj[:spec] += objname
+    obj[:spec] += objname.gsub(/^\./,'#')
     parse_block(statement.last.last, owner: obj)
   rescue YARD::Handlers::NamespaceMissingError
   end
@@ -18,7 +18,13 @@ class RSpecContextHandler < YARD::Handlers::Ruby::Base
   handles method_call(:context)
   
   def process
-    parse_block(statement.last.last, owner: owner)
+    if owner
+      owner[:context] ||= []
+      owner[:context] << statement.parameters.first.jump(:string_content).source
+    end
+    r = parse_block(statement.last.last, owner: owner)
+    owner[:context].pop if owner
+    r
   rescue YARD::Handlers::NamespaceMissingError
   end
 end
@@ -31,17 +37,26 @@ class RSpecItHandler < YARD::Handlers::Ruby::Base
     obj = P(owner[:spec])
     return if obj.is_a?(Proxy)
     
-    specs = (obj[:specifications] ||= [])
+
+    context = owner[:context]||[]
+
+    obj[:specifications] ||= {}
+    obj[:specifications][:all] ||= []
+    ob = obj[:specifications]
+    context.each do |co|
+      ob[co] ||= {}
+      ob[co][:all] ||= []
+      ob = ob[co]
+    end
     
     name = statement.parameters.first.jump(:string_content).source
     
-    unless specs.find{ |spec| spec[:name] == name }    
-      (obj[:specifications] ||= []) << {
+    ob[:all] << {
         name: name,
         file: statement.file,
         line: statement.line,
         source: statement.last.last.source.chomp
       }
-    end
+    obj[:specifications]
   end
 end
